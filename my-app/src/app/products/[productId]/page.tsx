c82a506e8408d582a65f47ee96c7ee1bfa8e2952
Swati Tanu <swatitani01@gmail.com>
2025-09-10 17:03:21 +0530

    Custom Not Found Page

--- my-app/src/app/products/[productId]/page.tsx
+++ my-app/src/app/products/[productId]/page.tsx
@@ -1,8 +1,13 @@
 // localhost:3000/products/{productId}: localhost:3000/products/1 or localhost:3000/products/iphone
 // use params to display dynamic content
+import { notFound } from "next/navigation";
 export default async function ProductDetails({ params,
  }: { 
    params: Promise<{productId: string}> }) {
    const productId = (await params).productId 
+   // Using Custom Not Found Page
+   if(parseInt(productId)>1000){
+         notFound();
+       }
    return <h1>Details about the product {productId}</h1>; 
 }
--- my-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ my-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,14 @@
 // localhost:3000/products/{productId}/reviews/{reviewId}
+import { notFound } from "next/navigation";
 export default async function ProductReviews({ params,
  }: { 
    params: Promise<{productId: string; reviewId: string}> }) {
     const {productId, reviewId} = await params
+
+    // Using Custom Not Found Page With Personalised Message
+    if(parseInt(reviewId)>1000){
+      notFound();
+    }
+
    return <h1>Review {reviewId} for Product {productId}</h1>; 
 }
