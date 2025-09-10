a6c0ec3c6c3497507f78ee25a21652a834b42fdd
Swati Tanu <swatitani01@gmail.com>
2025-09-10 18:37:40 +0530

    Layout

--- my-app/src/app/(auth)/register/page.tsx
+++ my-app/src/app/(auth)/register/page.tsx
@@ -1,5 +1,5 @@
 // Route Groups: (foldername)
-// This way "auth" won't come in URL. For better organisation of code and exclude from url path
+// This way "auth" won't come in URL. For better organisation of code and exclude from url path.
 
 export default function Register(){
     return <h1>Resigter</h1>
