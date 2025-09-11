cb948b05c3ae868aeffbe1522f503f9a7ee1bfbe
Swati Tanu <swatitani01@gmail.com>
2025-09-11 11:12:38 +0530

    Titile Metadata

--- my-app/src/app/blog/page.tsx
+++ my-app/src/app/blog/page.tsx
@@ -1,5 +1,11 @@
 // Static nested routes
-
+import {Metadata} from "next";
+export const metadata: Metadata = {
+  // title: "Blog"
+  title:{
+    absolute:"Just Blog" // Free from paren
+  }
+}
 // localhost:3000/blog
 export default function Blog() {
   return <h1>My Blog!</h1>;
