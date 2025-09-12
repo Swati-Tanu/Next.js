aac0ccaf422225fbecb5a88dfe0595d0e488a7a7
Swati Tanu <swatitani01@gmail.com>
2025-09-12 19:18:21 +0530

    Dynamic Rendering

--- my-app/src/app/contact/page.tsx
+++ my-app/src/app/contact/page.tsx
@@ -1,6 +1,12 @@
+import { cookies } from "next/headers";
+
 // Server Component
-export default function ContactPage() {
+export default async function ContactPage() {
   console.log("Contact server component");
+  // Dynamic Rendering
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
 //   return <h1>Contact page</h1>;
  return <h1>Contact page {new Date().toLocaleTimeString()}</h1>;
 }
