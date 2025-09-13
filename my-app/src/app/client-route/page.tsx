29e7888cf7e25b3df23cc7f46f65ae03b3b33d6e
Swati Tanu <swatitani01@gmail.com>
2025-09-13 16:11:37 +0530

    third party packages

---
+++ my-app/src/app/client-route/page.tsx
@@ -0,0 +1,36 @@
+// Importing server code in client component
+
+"use client";
+
+// import { serverSideFunction } from "../utils/page";
+
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function ClientRoutePage() {
+  // const result = serverSideFunction();
+  // return <h1>Client Route {result}</h1>;
+  const settings = {
+    dots: true,
+  };
+  return (
+    <div className="image-slider-container">
+      <Slider {...settings}>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+      </Slider>
+    </div>
+  );
+}
